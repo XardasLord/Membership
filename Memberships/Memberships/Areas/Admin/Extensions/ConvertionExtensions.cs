3e9fda0211d525e5f6e65582b65809c1ee339d87
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-02-28 23:20:57 +0100

    Controller and views for a Product model.

---
+++ Memberships/Memberships/Areas/Admin/Extensions/ConvertionExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Memberships.Areas.Admin.Models;
+using Memberships.Entities;
+using Memberships.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Memberships.Areas.Admin.Extensions
+{
+    public static class ConvertionExtensions
+    {
+        public static async Task<IEnumerable<ProductModel>> ConvertAsync(this IEnumerable<Product> products, ApplicationDbContext db)
+        {
+            if (products.Count().Equals(0))
+                return new List<ProductModel>();
+
+            var texts = await db.ProductLinkTexts.ToListAsync();
+            var types = await db.ProductTypes.ToListAsync();
+
+            return products.Select(p => new ProductModel
+            {
+                Id = p.Id,
+                Title = p.Title,
+                Description = p.Description,
+                ImageUrl = p.ImageUrl,
+                ProductLinkTextId = p.ProductLinkTextId,
+                ProductTypeId = p.ProductTypeId,
+                ProductLinkTexts = texts,
+                ProductTypes = types
+            });
+        }
+
+        public static async Task<ProductModel> ConvertAsync(this Product product, ApplicationDbContext db)
+        {
+            var texts = await db.ProductLinkTexts.FirstOrDefaultAsync(p => p.Id.Equals(product.ProductLinkTextId));
+            var types = await db.ProductTypes.FirstOrDefaultAsync(p => p.Id.Equals(product.ProductTypeId));
+
+            var productModel =  new ProductModel
+            {
+                Id = product.Id,
+                Title = product.Title,
+                Description = product.Description,
+                ImageUrl = product.ImageUrl,
+                ProductLinkTextId = product.ProductLinkTextId,
+                ProductTypeId = product.ProductTypeId,
+                ProductLinkTexts = new List<ProductLinkText>(),
+                ProductTypes = new List<ProductType>()
+            };
+
+            productModel.ProductLinkTexts.Add(texts);
+            productModel.ProductTypes.Add(types);
+
+            return productModel;
+        }
+    }
+}
