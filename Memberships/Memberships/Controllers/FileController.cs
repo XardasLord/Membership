cd3d0131099b4e311b5c8348fa33709a1f2c80c6
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-04-11 00:16:54 +0200

    Server File Upload for Admins only

---
+++ Memberships/Memberships/Controllers/FileController.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Memberships.Controllers
+{
+    [Authorize]
+    public class FileController : Controller
+    {
+        // GET: File
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            var path = Server.MapPath("~/Content/Files");
+            var dir = new DirectoryInfo(path);
+            var files = dir.EnumerateFiles().Where(f => f.Extension != ".gitignore").Select(f => f.Name);
+
+            return View(files);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            var path = Path.Combine(Server.MapPath("~/Content/Files/"), file.FileName);
+            var data = new byte[file.ContentLength];
+            file.InputStream.Read(data, 0, file.ContentLength);
+            using (var sw = new FileStream(path, FileMode.Create))
+            {
+                sw.Write(data, 0, data.Length);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
